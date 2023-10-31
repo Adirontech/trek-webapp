8117acf6e9e2f0cb8807984055328f405768edaf
Cameron Turner <cturner@CCON-LT24.careerconcepts.local>
2023-10-30 21:53:30 -0400

    small fixes

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {Component, isValidElement} from "react";
 
 class Login extends Component {
     constructor() {
@@ -8,29 +8,84 @@ class Login extends Component {
             password: "",
             passwordconf: "",
             createAccount: false,
+            errors: {},
         };
     }
 
-    signIn = (e) => {
+    changeForm = (e) => {
+        const createAccount = this.state.createAccount;
         this.setState({
-            createAccount: false
-        })
-    }
-
-    create = (e) => {
-        this.setState({
-            createAccount: true
+            createAccount: !createAccount,
+            username: "",
+            password: "",
+            passwordconf: "",
+            errors: {},
         });
     };
 
     change = (e) => {
         const {name, value} = e.target;
+        let errors = {};
         this.setState({
             [name]: value
         });
+        if(name === "username"){
+            errors["username"] = "";
+        }
+        if(name === "password"){
+            errors["password"] = "";
+        }
+        if(name === "passwordconf"){
+            const pw = this.state.password;
+            errors["passwordconf"] = "";
+            if(value === pw){
+                errors["match"] = "";
+            }
+        }
+        this.setState({
+            errors: errors
+        })
     };
 
+    validate() {
+        let isValid = true;
+        let errors = {};
+        let user = this.state.username;
+        let pw = this.state.password;
+        let pwconf = this.state.passwordconf;
+        if(!user){
+            isValid = false;
+            errors["username"] = "Please enter a username";
+        }
+        if(!pw){
+            isValid = false;
+            errors["password"] = "Please enter a password";
+        }
+        if(!pwconf){
+            isValid = false;
+            errors["passwordconf"] = "Please confirm your password";
+        }
+        if(pw !== pwconf){
+            isValid = false;
+            errors["match"] = "Passwords do not match";
+        }
+        this.setState({
+            errors: errors
+        });
+        return isValid;
+    }
+
     submit = (e) => {
+        const isCreate = this.state.createAccount;
+        if(isCreate){
+            if(this.validate()){
+
+            }
+        }else{
+            if(this.validate()){
+
+            }
+        }
         e.preventDefault();
         console.log("Logged In");
     } 
@@ -39,7 +94,6 @@ class Login extends Component {
         const isCreate = this.state.createAccount;
         if(isCreate){
             return(
-                <div className="">
                     <div className="bg-black w-full">
                         <div className=" flex items-center justify-center bg-black bg-login bg-cover h-screen w-screen">
                             <div className=" flex flex-col items-center justify-center">
@@ -56,39 +110,42 @@ class Login extends Component {
                                                 value={this.state.username}
                                                 onChange={this.change}
                                             />
+                                            <div className=" text-xs text-red-500">{this.state.errors.username}</div>
                                         </div>
                                         <div className="mb-4">
                                             <label className="block text-gray-600 text-sm">Password</label>
                                             <input
                                                 className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-green-400"
-                                                type="text"
+                                                type="password"
                                                 name="password"
                                                 value={this.state.password}
                                                 onChange={this.change}
                                             />
+                                            <div className=" text-xs text-red-500">{this.state.errors.password}</div>
+
                                         </div>
                                         <div className="mb-4">
                                             <label className="block text-gray-600 text-sm">Password Confirmation</label>
                                             <input
                                                 className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-green-400"
-                                                type="text"
-                                                name="passwordConf"
+                                                type="password"
+                                                name="passwordconf"
                                                 value={this.state.passwordconf}
                                                 onChange={this.change}
                                             />
+                                            <div className=" text-xs text-red-500">{this.state.errors.passwordconf}</div>
+                                            <div className=" text-xs text-red-500">{this.state.errors.match}</div>
                                         </div>
                                         <button className="w-full bg-green-600 text-white p-2 rounded hover:bg-green-700" type="submit">Log In</button>
-                                        <button className="text-xs text-left hover:text-green-900" onClick={this.signIn}>Sign In</button>
+                                        <button className="text-xs text-left hover:text-green-900 relative top-2" onClick={this.changeForm}>Sign In</button>
                                     </form>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
             );
         }else {
             return(
-                <div className="">
                     <div className="bg-black w-full">
                         <div className=" flex items-center justify-center bg-black bg-login bg-cover h-screen w-screen">
                             <div className=" flex flex-col items-center justify-center">
@@ -104,31 +161,32 @@ class Login extends Component {
                                                 name="username"
                                                 value={this.state.username}
                                                 onChange={this.change}
+                                                
                                             />
+                                            <div className=" text-xs text-red-500">{this.state.errors.username}</div>
+
                                         </div>
                                         <div className="mb-4">
                                             <label className="block text-gray-600 text-sm">Password</label>
                                             <input
                                                 className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-green-400"
-                                                type="text"
+                                                type="password"
                                                 name="password"
                                                 value={this.state.password}
                                                 onChange={this.change}
                                             />
+                                            <div className=" text-xs text-red-500">{this.state.errors.password}</div>
                                         </div>
                                         <button className="w-full bg-green-600 text-white p-2 rounded hover:bg-green-700" type="submit">Log In</button>
-                                        <button className="text-xs text-left hover:text-green-900" onClick={this.create}>Create Account</button>
+                                        <button className="text-xs text-left hover:text-green-900 relative top-2" onClick={this.changeForm}>Create Account</button>
                                     </form>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
             );
         }
-    
-    }
-    
+    }  
 }
 
 export default Login;
