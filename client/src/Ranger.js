3b17a64b3bb6e091f1ea697f09e7a221b8760a99
Johnny Puskar <johnnypuskar@yahoo.com>
2023-10-31 15:09:20 -0400

    Created basic empty ranger page (no components yet since exact layout and contents are TBD)

---
+++ client/src/Ranger.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class Ranger extends Component {
+    render() {
+        return (
+            <div className="bg-gradient-to-b from-green-800 to-green-950 h-screen">
+                <p>
+                    Ranger view data, coming soon!
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Ranger;
+
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./public/index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
