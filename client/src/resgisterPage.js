8f374750f48c2b0594d8ec05fdaf6a5d53f63e44
Quincy Myles Jr <qmjr@quincys-mbp.wireless.rit.edu>
2023-10-31 17:39:13 -0400

    completed basic registartion page, a bit blank

---
+++ client/src/resgisterPage.js
@@ -0,0 +1,21 @@
+
+
+const registerPage = () => {
+    return (
+        <div className="registerPage">
+            <div className="title">
+
+            Registration Page :)
+
+            </div>
+            
+        </div>
+        
+
+
+
+    );
+
+}
+
+export default registerPage; 
