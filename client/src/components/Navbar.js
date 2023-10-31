cbb911166405688ce94534ebc661500653f7e61c
Heather Moses <heather-moses@outlook.com>
2023-10-31 16:41:41 -0400

    new branch name and bolded navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <nav className="flex justify-between items-center">
             {/* main nav - centered */}
-            <div className='text-center text-xl text-white flex-1'>
+            <div className='text-center text-xl text-white flex-1 font-medium'>
                 <a className="mr-8" href="">Home</a>
                 <a className="mr-8" href="">About</a>
                 <a href="">Trip Registration</a>
