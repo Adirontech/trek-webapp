2c1ddacf3a79bfd21b2034da2779dd89bff2d364
Johnny Puskar <johnnypuskar@yahoo.com>
2023-11-06 13:35:48 -0500

    functional ranger component

--- client/src/Ranger.js
+++ client/src/Ranger.js
@@ -1,15 +1,13 @@
-import React, {Component} from "react";
+import React from "react";
 
-class Ranger extends Component {
-    render() {
-        return (
-            <div className="bg-gradient-to-b from-green-800 to-green-950 h-screen">
-                <p>
-                    Ranger view data, coming soon!
-                </p>
-            </div>
-        );
-    }
+const Ranger = () => {
+     return (
+        <div className="bg-gradient-to-b from-green-800 to-green-950 h-screen">
+            <p>
+                Ranger view data, coming soon!
+            </p>
+        </div>
+    );
 }
 
 export default Ranger;
