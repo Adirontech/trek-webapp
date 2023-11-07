01fcef37be0bb55ef3aa4a4b0a7eacecdebb4c03
Johnny Puskar <johnnypuskar@yahoo.com>
2023-11-07 18:10:03 -0500

    things are now spelled correctly using the Queen's English

---
+++ client/src/Register.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Register = () => {
+    return (
+        <div className="Register">
+            <div className="title">
+
+                Registration Page :)
+
+            </div>
+            
+        </div>
+        
+
+
+
+    );
+
+}
+
+export default Register; 
