8cd8cc71dd7d16f0859eb0e0a2c7789e1b0ec644
Heather Moses <heather-moses@outlook.com>
2023-11-14 22:11:03 -0500

    small fixes

--- api/sql/tripSQL/initialize.sql
+++ api/sql/tripSQL/initialize.sql
@@ -2,13 +2,13 @@ DROP TABLE IF EXISTS Trips CASCADE;
 
 CREATE TABLE Trips (
     id SERIAL PRIMARY KEY,
-    leader INT,
-    date DATETIME,
-    start INT,
+    leader INT NOT NULL,
+    date DATETIME NOT NULL,
+    start INT NOT NULL,
     destinations INT[] REFERENCES POI(id),
     purpose TEXT,
     duration INT,
     party_size INT,
-    FOREIGN KEY (leader) REFERENCES UserData(id),
+    FOREIGN KEY (leader) REFERENCES User(id),
     FOREIGN KEY (start) REFERENCES POI(id)
 )
