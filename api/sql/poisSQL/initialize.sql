c42247961eb7f713307a85cbeb3c836914c3a857
Heather Moses <heather-moses@outlook.com>
2023-11-15 23:50:22 -0500

    create enum for poi type

---
+++ api/sql/poisSQL/initialize.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS POIs CASCADE;
+
+CREATE TYPE poi_type_enum AS ENUM ('Trailhead', 'Trail Location')
+
+CREATE TABLE POIs (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    type poi_type_enum
+)
