13292f48ed67fd23a4b12b97246b58b10b6a9e76
Heather Moses <heather-moses@outlook.com>
2024-01-23 10:26:38 -0500

    navbar is actually centered now

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,15 +2,15 @@ import profileIcon from '../assets/images/profile-icon.png';
 
 const Navbar = () => {
     return (
-        <nav className="flex justify-between items-center">
+        <nav className="relative flex justify-between items-center">
             {/* main nav - centered */}
             <div className='text-center text-xl text-white flex-1 font-medium'>
-                <a className="mr-8" href="">Home</a>
-                <a className="mr-8" href="">About</a>
-                <a href="">Trip Registration</a>
+                <a className="mx-4" href="">Home</a>
+                <a className="mx-4" href="">About</a>
+                <a className="mx-4" href="">Trip Registration</a>
             </div>
             {/* profile - right */}
-            <div className=''>
+            <div className="absolute right-0">
                 <a href=""><img src={profileIcon} height={40} width={40}/></a>
             </div>
         </nav>
