451f9ee709e45f0f68eb7ad46d9b1b0e400cc0b8
Heather Moses <heather-moses@outlook.com>
2024-02-10 17:34:18 -0500

    Added test data from craig

--- api/init/03-poisInit.sql
+++ api/init/03-poisInit.sql
@@ -1,9 +1,10 @@
 DROP TABLE IF EXISTS POIs CASCADE;
 
-CREATE TYPE poi_type_enum AS ENUM ('Trailhead', 'Trail Location');
+CREATE TYPE poi_type_enum AS ENUM ('Trailhead', 'Peak', 'Scenic', 'Lodge', 'Leanto');
 
 CREATE TABLE POIs (
     id SERIAL PRIMARY KEY,
     name TEXT,
-    type poi_type_enum
+    type poi_type_enum,
+    wilderness_area TEXT
 );
