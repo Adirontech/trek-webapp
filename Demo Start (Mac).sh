551f224e4c0c506b7dd3f8891fb0bc0fc3cf7adf
Jonah Micek <jonah@designhouse.design>
2024-02-19 02:32:30 -0500

    Hopefully fixed error(s) where the Mac setup -.command files weren't being set to be executables.

--- Demo Start (Mac).sh
+++ Demo Start (Mac).sh
@@ -4,10 +4,13 @@
 cd demo_start_files/
 
 # Start Docker DB
+chmod +x start_docker_db.command # If regular chmod doesn't work, add "sudo " in front of it & see if that fixes it.
 open -a Terminal.app start_docker_db.command
 
 # Start API
+chmod +x start_api.command # If regular chmod doesn't work, add "sudo " in front of it & see if that fixes it.
 open -a Terminal.app start_api.command
 
 # Start Client
+chmod +x start_client.command # If regular chmod doesn't work, add "sudo " in front of it & see if that fixes it.
 open -a Terminal.app start_client.command
