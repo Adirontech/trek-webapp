0dc0426fd3541d20da01b6247c6f12e5cdbd34bc
Heather Moses <heather-moses@outlook.com>
2024-03-04 22:06:38 -0500

    remove duplicate routing

--- client/src/index.js
+++ client/src/index.js
@@ -6,12 +6,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // Import CSS file for styling
-import Login from './pages/Login'; // Import Login component
-import Home from './pages/Home'; // Import Home component
-import Ranger from './pages/Ranger'; // Import Ranger component
-import Register from './pages/Register'; // Import Register component
 import reportWebVitals from './reportWebVitals'; // Import utility for measuring web vitals
-import { BrowserRouter, Routes, Route } from "react-router-dom"; // Import React Router components for routing
+import App from './App';
 
 // Create a root instance for rendering React components
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -19,17 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // Render the main application component
 root.render(
   <React.StrictMode>
-    {/* Wrap the application in StrictMode for enhanced error detection */}
-    <BrowserRouter>
-      {/* Provide routing functionality using BrowserRouter */}
-      <Routes>
-        {/* Define route configurations */}
-        <Route path='ranger' element={<Ranger/>} /> {/* Render Ranger component for '/ranger' route */}
-        <Route path='login' element={<Login/>} /> {/* Render Login component for '/login' route */}
-        <Route path='' element={<Home/>} /> {/* Render Home component for default route '/' */}
-        <Route path='register' element={<Register />} /> {/* Render Register component for '/register' route */}
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
