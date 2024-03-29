8c1259382a30ced29ee987dcd5643a34f75a218b
cameron turner <cpt2162@rit.edu>
2024-03-29 15:59:49 -0400

    w

--- client/src/components/POIDataFilter.js
+++ client/src/components/POIDataFilter.js
@@ -17,8 +17,18 @@ import POISelect from "./POISelect";
  */
 const POIDataFilter = (props) => {
     const [selected, setSelected] = useState([]); // State for selected POIs
-    const [min, setMin] = useState(0); // State for minimum encounters
-    const [max, setMax] = useState(100); // State for maximum encounters
+    const [filter, setFilter] = useState({
+        min: 0,
+        max: 100,
+        trailhead: '',
+        peak: '',
+        scenic: '',
+        lodge: '',
+        leanto: '',
+        from: '',
+        to: '',
+        selected: []
+    }); // State for filter options
 
     // // useEffect hook to handle changes in selected POIs
     // useEffect(() => {
@@ -41,10 +51,19 @@ const POIDataFilter = (props) => {
         console.log(selected)// Calling handleChange function with selected POIs
     };
 
-    const handleSliderChange = (range) => {
-        setMin(range[0]);
-        setMax(range[1]);
-    }
+    const handleChange = (e) => { // Function to handle change in filter options 
+        const { name, value, checked, id } = e.target;
+        console.log(name, value);
+        if(name === "slider") {
+            setFilter({ ...filter, min: value[0], max: value[1] });
+            return;
+        } else if(name === "area") {
+            console.log("area", value);
+            setFilter({ ...filter, [id]: checked });
+        } else {
+            setFilter({ ...filter, [name]: value });
+        }
+    };
 
     return (
         <div className="bg-white pt-4 pl-2 sm:pl-4 pr-2 sm:pr-4 pb-3 rounded-lg shadow-md xl:w-2/4 lg:w-7/12 md:w-4/5 w-full">
@@ -56,53 +75,54 @@ const POIDataFilter = (props) => {
                     <div className="flex flex-row sm:flex-col justify-between items-center w-full sm:w-1/4 md:pr-2 pr-5 sm:h-20">
                         <div className="flex flex-row w-full justify-around sm:justify-between">
                             <label className="text-lg self-end font-bold">From: </label>
-                            <input type="date" className="h-8 border-2 rounded-md 2xl:w-32 sm:w-20" />
+                            <input type="date" onChange={handleChange} name="from" value={filter.from} className="h-8 border-2 rounded-md 2xl:w-32 sm:w-20" />
                         </div>
                         <div className="flex flex-row w-full justify-around sm:justify-between">
                             <label className="text-lg self-end font-bold">To: </label>
-                            <input type="date" className="h-8 border-2 rounded-md 2xl:w-32 sm:w-20" />
+                            <input type="date" onChange={handleChange} name="to" value={filter.to} className="h-8 border-2 rounded-md 2xl:w-32 sm:w-20" />
                         </div>
                     </div>
                     <div className="flex flex-col w-full sm:w-3/4 pt-2 sm:pt-0">
-                        <div className="flex flex-row w-full justify-between md:pl-6">
+                        <fieldset className="flex flex-row w-full justify-between md:pl-6">
                             <div className="flex flex-row items-center">
                                 <label className="text-sm pr-2">Trailhead</label>
-                                <button type="radio" className="border-2 w-6 h-6 rounded-md"></button>
+                                <input type="checkbox" onChange={handleChange} name="area" id="trailhead" className="border-2 w-6 h-6 rounded-md"></input>
                             </div>
                             <div className="flex flex-row items-center">
                                 <label className="text-sm pr-2">Peak</label>
-                                <button type="radio" className="border-2 w-6 h-6 rounded-md"></button>
+                                <input type="checkbox" onChange={handleChange} name="area" id="peak" value={filter.peak} className="border-2 w-6 h-6 rounded-md"></input>
                             </div>
                             <div className="flex flex-row items-center">
                                 <label className="text-sm pr-2">Scenic</label>
-                                <button type="radio" className="border-2 w-6 h-6 rounded-md"></button>
+                                <input type="checkbox" onChange={handleChange} name="area" id="scenic" value={filter.scenic} className="border-2 w-6 h-6 rounded-md"></input>
                             </div>
                             <div className="flex flex-row items-center">
                                 <label className="text-sm pr-2">Lodge</label>
-                                <button type="radio" className="border-2 w-6 h-6 rounded-md"></button>
+                                <input type="checkbox" onChange={handleChange} name="area" id="lodge" value={filter.lodge} className="border-2 w-6 h-6 rounded-md"></input>
                             </div>
                             <div className="flex flex-row items-center">
                                 <label className="text-sm pr-2">Leanto</label>
-                                <button type="radio" className="border-2 w-6 h-6 rounded-md"></button>
+                                <input type="checkbox" onChange={handleChange} name="area" id="leanto" value={filter.leanto} className="border-2 w-6 h-6 rounded-md"></input>
                             </div>
-                        </div>
-                        <div className="flex flex-row justify-between items-center w-full h-9 mt-3">
+                        </fieldset>
+                        <div className="flex flex-row justify-between items-center w-full h-9 pt-6">
                             <div className="flex flex-col w-1/2 md:pl-6">
                                 <Slider
                                     range
                                     min={0}
                                     max={100}
                                     marks={marks}
+                                    name="slider"
                                     defaultValue={[0, 100]}
-                                    value={[min, max]}
+                                    value={[filter.min, filter.max]}
                                     pushable
-                                    onChange={handleSliderChange}
+                                    onChange={handleChange}
                                 />
                                 <label className="text-xs sm:text-sm text-center ">Number of Encounters</label>
                             </div>
                             <div className="flex flex-row w-1/2 md:pl-0 lg:pl-4 xl:pl-8 justify-around">
-                                <label className="text-md sm:text-lg font-bold">Min: {min}</label>
-                                <label className="text-md sm:text-lg font-bold">Max: {max}</label>
+                                <label className="text-md sm:text-lg font-bold">Min: {filter.min}</label>
+                                <label className="text-md sm:text-lg font-bold">Max: {filter.max}</label>
                             </div>
                             <div className="flex flex-row justify-end w-1/4">
                                 <button className="bg-green font-bold w-24 h-8 relative rounded">
