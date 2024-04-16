9a25e0a85b2ee7a430ad4b99b031b64f9cf593f8
johnnypuskar@yahoo.com <Johnny Puskar>
2024-04-16 00:10:47 -0400

    Bugfix for inability to read .env file from browser.

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,9 +1,5 @@
-// Use path & dotenv to ensure the API process can access root -.env variables
-const path = require('path');
-console.log(require('dotenv').config({path: path.resolve(__dirname, '../../../.env')}));
-
-// Grab Necessary Config Information From Root .env file
-const url = process.env.API_URL // The API URL
+/// Grab Necessary Config Information From React App .env Data
+const url = process.env.REACT_APP_API_URL // The API URL
 
 // Export completed API URL
 module.exports = {
