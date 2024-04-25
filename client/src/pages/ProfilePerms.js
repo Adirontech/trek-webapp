3bc91254d9a74c2fdd8ee9e3d29412973cc21e7c
Cameron Turner <cturner@CCON-LT24.careerconcepts.local>
2024-04-25 14:00:11 -0400

    added navbar

--- client/src/pages/ProfilePerms.js
+++ client/src/pages/ProfilePerms.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
 
 import '../assets/stylesheets/App.css'; // Importing the CSS file for styling
 import { useNavigate } from 'react-router-dom'; // Importing useNavigate hook for navigation
@@ -65,30 +66,30 @@ const ProfileTerms = () => {
     }
 
     return(
-        <div className="bg-black w-full">
-        {/* Login form */}
-            <div className=" flex items-center justify-center bg-black bg-login bg-cover h-screen w-screen">
-                <div className=" flex flex-col items-center justify-center">
-                    <div className="bg-white p-6 rounded-lg shadow-md w-72">
-                        <h2 className="text-2xl font-semibold mb-4 text-center">Make Land Allocator</h2>
-                        <form className="" onSubmit={setAllocator}>
-                            <div className="mb-4">
-                                <label className="block text-gray-600 text-sm">Username</label>
-                                <input
-                                    className="w-full p-2 border border-gray rounded focus:outline-none focus:border-green-400"
-                                    type="text"
-                                    name="allocator_username"
-                                    value={allocatorUser}
-                                    onChange={(e) => setAllocatorUser(e.target.value)}
-                                />
-                            </div>
-                            <div className="pb-2 text-xs text-red">{error}</div>
-                            <button className="w-full bg-green text-white p-2 rounded hover:bg-green" type="submit">Submit</button>
-                        </form>
+            <div className="h-screen bg-cover bg-login bg-center bg-fixed bg-no-repeat hero p-8">
+                <Navbar />
+                <div className="flex items-center justify-center h-full w-full">
+                    <div className="flex flex-col items-center justify-center">
+                        <div className="bg-white p-6 rounded-lg shadow-md w-72">
+                            <h2 className="text-2xl font-semibold mb-4 text-center">Make Land Allocator</h2>
+                            <form className="" onSubmit={setAllocator}>
+                                <div className="mb-4">
+                                    <label className="block text-gray-600 text-sm">Username</label>
+                                    <input
+                                        className="w-full p-2 border border-gray rounded focus:outline-none focus:border-green-400"
+                                        type="text"
+                                        name="allocator_username"
+                                        value={allocatorUser}
+                                        onChange={(e) => setAllocatorUser(e.target.value)}
+                                    />
+                                </div>
+                                <div className="pb-2 text-xs text-red">{error}</div>
+                                <button className="w-full bg-green text-white p-2 rounded hover:bg-green" type="submit">Submit</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
     );
 }
 
